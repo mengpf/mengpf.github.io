38a0bc48cd35c6244b3910e6f48f155f484aed29
mengpf <88969189@qq.com>
2023-04-02 01:45:24 +0800

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -4,19 +4,10 @@ export const zhSidebar = sidebar({
   "/": [
     "",
     {
-      text: "如何使用",
-      icon: "creative",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
       text: "文章",
       icon: "note",
       prefix: "posts/",
       children: "structure",
     },
-    "intro",
-    "slides",
   ],
 });
